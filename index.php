ede6c886c4f65faa7cc08ec5af07013486c7e130
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-22 00:53:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>CMS Project - Lidya Nuramo</title>
-  </head>
-  <body>
     <nav class="nav bg-transparent" >
       <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
     </nav>
@@ -20,6 +18,11 @@
           <a class="nav-link" href="Guest Home">Log in as a Guest</a>
         </li>
     </ul>
+  </head>
+  <body >
+    <div class="p-3 mb-2 bg-secondary text-white">
+      Log in.
+    </div>
   </body>
   <footer>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>

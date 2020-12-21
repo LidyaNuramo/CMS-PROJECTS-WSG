e8a1b3b57c1a6d4a00721fa3f78f82e938f87d3c
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-22 00:40:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,20 +9,23 @@
     <title>CMS Project - Lidya Nuramo</title>
   </head>
   <body>
-    <ul class="nav justify-content-end">
-      <li class="nav-item" text-align="left">
-        <a class="nav-link active" aria-current="page" href="#" >Lidya Nuramo CMS project</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Create an account</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Guest Home">Log in as a Guest</a>
-      </li>
-      <!--li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li-->
+    <nav class="nav">
+      <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
+      <ul class="nav justify-content-end">
+        <!--li class="nav-item" text-align="left">
+          <a class="nav-link active" aria-current="page" href="#" >Lidya Nuramo CMS project</a>
+        </li-->
+        <li class="nav-item">
+          <a class="nav-link" href="#">Create an account</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Guest Home">Log in as a Guest</a>
+        </li>
+        <!--li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li-->
     </ul>
+    </nav>
   </body>
   <footer>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>

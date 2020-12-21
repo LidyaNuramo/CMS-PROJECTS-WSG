64042fb1ac27ea0a89f2489d3d80c87af4fff449
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-22 00:54:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>CMS Project - Lidya Nuramo</title>
-    <nav class="nav bg-transparent" >
-      <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
-    </nav>
-    <ul class="nav justify-content-end bg-transparent">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Create an account</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Guest Home">Log in as a Guest</a>
-        </li>
-    </ul>
   </head>
   <body >
     <div class="p-3 mb-2 bg-secondary text-white">
+      <nav class="nav bg-transparent" >
+        <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
+      </nav>
+      <ul class="nav justify-content-end bg-transparent">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Create an account</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Guest Home">Log in as a Guest</a>
+          </li>
+      </ul>
       Log in.
     </div>
   </body>

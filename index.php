25d86a2ccd44e458dac2e2a3ac1b5f8278b785d8
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-22 01:22:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,18 @@
       </li>
     </ul>
     <div class="p-3 mb-2 bg-secondary text-white">
-      Log in now.
+      <form action="DB/process.php?action=login" method="GET">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   </body>
   <footer>

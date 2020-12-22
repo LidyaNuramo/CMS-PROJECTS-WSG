35fde13d339920ca7620ede8e036681283cf8e46
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-22 02:40:18 +0100

    Update main.php

--- DB/main.php
+++ DB/main.php
@@ -1,4 +1,5 @@
 <?php
+  function init_connection(){
     $dbhost="localhost"; 
     $dbuser="root"; 
     $dbpassword="VhFtn4kUqaa6"; 
@@ -10,13 +11,15 @@
       echo "Error: " . mysqli_connect_error() . PHP_EOL;
       exit;
     }
+  }
     
   function close_connection(){
     mysqli_close($connection);
   }
 
   function get_rows($tablename){
-    $query = "SELECT * FROM " . $tablename;
+    init_connection();
+    $query = "SELECT * FROM ".$tablename;
     echo $tablename;
     $result = mysqli_query($connection, $query) or die ("DB error: $dbname".$tablename);
     close_connection();
@@ -24,6 +27,7 @@
   }
 
   function select_row($tablename, $id){
+    init_connection();
     $query = "SELECT * FROM ".$tablename." where id='".$id."'";
     $result = mysqli_query($connection, $query) or die ("DB error: $dbname");
     close_connection();

0634d12d5746ad666fa0088309e45cb4592cc555
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-30 02:22:12 +0100

    Update process.php

--- DB/process.php
+++ DB/process.php
@@ -4,11 +4,11 @@ require_once('main.php');
 if(!empty($_GET['action'])){
 	switch($_GET['action']){
 		case 'login':
-	       $email=$_POST['email'];
+	           $email=$_POST['email'];
 		   $password=$_POST['password'];
-	       $where['email']= '="'.$email.'"';
+	       	   $where=$email;
 		   $database=new Database();
-		   $user=$database->select_row("users","*",$where);
+		   $user=$database->select_row("users","*","email",$where);
 		   if($user['password']==$password){
 				session_start();
 				$_SESSION['username']=$user['FirstName'];

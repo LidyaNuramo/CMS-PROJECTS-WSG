2b6d1add3554aa7d49190b98f06de46644b14b67
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-30 02:52:24 +0100

    Update index.php

--- Home/index.php
+++ Home/index.php
@@ -7,9 +7,9 @@
         <p><h2>Student Name: Lidya Nuramo</h2></p>
         <p><h2>Student ID: 030794</h2></p>
         <h3>
-            <p><a href="Exercise1">Exercies 1.</a></p>
-            <p><a href="Exercise2">Exercies 2.</a></p>
-            <p><a href="Exercise3">Exercies 3.</a></p>
+            <p><a href="../Exercise1">Exercies 1.</a></p>
+            <p><a href="../Exercise2">Exercies 2.</a></p>
+            <p><a href="../Exercise3">Exercies 3.</a></p>
         </h3>
     </body>
 </html>

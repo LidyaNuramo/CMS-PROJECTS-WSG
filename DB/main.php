734b01ba93c3ecb07eb1f7ab8ea51cc25d6109b5
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2020-12-30 02:22:38 +0100

    Update main.php

--- DB/main.php
+++ DB/main.php
@@ -1,37 +1,41 @@
 <?php
-  function init_connection(){
-    $dbhost="localhost"; 
-    $dbuser="root"; 
-    $dbpassword="VhFtn4kUqaa6"; 
-    $dbname="Webservers";
-    $connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
-    if (!$connection) {
-      echo " MySQL Connection error." . PHP_EOL;
-      echo "Errno: " . mysqli_connect_errno() . PHP_EOL;
-      echo "Error: " . mysqli_connect_error() . PHP_EOL;
-      exit;
-    }
-    else{
-      return $connection;
-    }
-  }
+
+class Database {
+	
+	function init_connection(){
+		$dbhost="localhost"; 
+		$dbuser="root"; 
+		$dbpassword="VhFtn4kUqaa6"; 
+		$dbname="Webservers";
+		if (!$connection) {
+			$connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+			echo " MySQL Connection error." . PHP_EOL;
+			echo "Errno: " . mysqli_connect_errno() . PHP_EOL;
+			echo "Error: " . mysqli_connect_error() . PHP_EOL;
+			exit;
+		}
+		else{
+			return $connection;
+		}
+	}
     
-  function close_connection(){
-    mysqli_close($connection);
-  }
+	function close_connection($aconnection){
+		mysqli_close($aconnection);
+	}
 
-  function get_rows($tablename){
-    $result_connection=init_connection();
-    $query = "SELECT * FROM ".$tablename;
-    $result = mysqli_query($result_connection, $query) or die ("DB error: $dbname");
-    close_connection();
-    return $result;
-  }
+	function get_rows($tablename){
+		$result_connection=init_connection();
+		$query = "SELECT * FROM ".$tablename;
+		$result = mysqli_query($result_connection, $query) or die ("DB error: $dbname");
+		close_connection($result_connection);
+		return $result;
+	}
 
-  function select_row($tablename, $id){
-    init_connection();
-    $query = "SELECT * FROM ".$tablename." where id='".$id."'";
-    $result = mysqli_query($connection, $query) or die ("DB error: $dbname");
-    close_connection();
-  }
+	function select_row($tablename, $columns ,$condition ,$where){
+		$result_connection=init_connection();
+		$query = "SELECT ".$columns." FROM ".$tablename." where ".$condition."='".$where."'";
+		$result = mysqli_query($connection, $query) or die ("DB error: $dbname");
+		close_connection($result_connection);
+	}
+}
 ?>

dfec37d81cbc7e8cc668a4f1a34cab4510ef4124
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-01 23:49:16 +0100

    Update process.php

--- DB/process.php
+++ DB/process.php
@@ -4,9 +4,9 @@ require_once('main.php');
 if(!empty($_GET['action'])){
 	switch($_GET['action']){
 		case 'login':
-	       	   $email=$_POST['email'];
+	       $email=$_POST['email'];
 		   $password=$_POST['password'];
-		   $where['Email']= '="'.$email.'"';
+		   $where['email']= '="'.$email.'"';
 		   $database=new Database();
 		   $user=$database->getRow("users","*",$where);
 		   if($user['password']==$password){
@@ -14,13 +14,45 @@ if(!empty($_GET['action'])){
 				$_SESSION['username']=$user['FirstName'];
 				$_SESSION['lastname']=$user['LastName'];
 				$_SESSION['userID']=$user['userID'];
-				header('../Location:Home');
+				header('Location:../Home');
 				break;
 		   }
 		   else{
 				header('Location:../index.php?action=no');
 		   }
 		   break;
+		case 'signup':
+			$fname=$_POST['firstname'];
+			$lname=$_POST['lastname'];
+			$email=$_POST['email'];
+			$phone=$_POST['phone'];
+			$password=$_POST['password'];
+			$database=new Database();
+			$where['Email']= '="'.$email.'"';
+	        $database=new Database();
+	        $results=$database->getRows("users","*",$where);
+			$num=1;
+			foreach($results as $result){
+			    $num=$num+1;
+			}
+			if ($num==1){
+				$data=array(
+					"userID"=>null,
+					"FirstName"=>$fname,
+					"LastName"=>$lname,
+					"AccountType"=>"standard",
+					"Email"=>$email,
+					"Phone"=>$phone,
+					"Password"=>$password,
+				);
+				$database->insertRows("users",$data);
+				$rr="Location: index.php?action=yes";
+				header($rr);
+				break;
+			}
+			$rr="Location: signup.php?action=no";
+			header($rr);
+			break;
    }
 }
 ?>

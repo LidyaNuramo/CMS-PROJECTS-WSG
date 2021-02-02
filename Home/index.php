3555115f058d6312c3363da3b6d8fde49ba78ac5
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 21:33:45 +0100

    Update index.php

--- Home/index.php
+++ Home/index.php
@@ -18,7 +18,7 @@ include('header.php');
 		
 		<div class="container-fluid tm-container-content tm-mt-60">
 			<div class="row mb-4">
-				<h2 class="col-6 tm-text-primary">
+				<h2 class="col-6 tm-text-primary" style="color: white;">
 					Latest Exercises
 				</h2>
 			</div>
@@ -43,10 +43,19 @@ include('header.php');
 				</div>
 				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
 					<figure class="effect-ming tm-video-item">
-						<img src="" alt="Image" class="img-fluid">
+						<img src="../Image/ex2.png" alt="Image" class="img-fluid">
 						<figcaption class="d-flex align-items-center justify-content-center">
 							<h2>Exercise 3</h2>
 							<a href="../Exercise3">View more</a>
+						</figcaption>                    
+					</figure>
+				</div>
+				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
+					<figure class="effect-ming tm-video-item">
+						<img src="../Image/ex4.jpg" alt="Image" class="img-fluid">
+						<figcaption class="d-flex align-items-center justify-content-center">
+							<h2>Exercise 4</h2>
+							<a href="#">View more</a>
 						</figcaption>                    
 					</figure>
 				</div>

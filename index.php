7c683bc804db7d6b11885436109b3ea2c03db0cf
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 03:10:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -69,6 +69,10 @@
 					<label for="exampleInputEmail1" style="font-size: 14pt;color: white;margin-left: 7%;margin-top: 5px;font-weight: bold;" class="col-sm-2 col-form-labels">Email address:</label>
 					<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" style="height: 40px;width: 75%;margin-left: 7%;" name="email" required>
 				</div>
+				<div class="form-row" style="font-size: 14pt;margin-top: 30px;">
+					<label for="exampleInputPassword1" style="font-size: 14pt;color: white;margin-left: 7%;margin-top: 5px;font-weight: bold;" class="col-sm-2 col-form-labels">Password:</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="height: 40px;width: 75%; margin-left: 7%;" name="password" required>
+				</div>
 			</form>
 		</div>
 		

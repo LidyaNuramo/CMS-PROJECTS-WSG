f24413288472e59e75d7e2a172ab15e54cb4c969
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 22:41:09 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -33,8 +33,8 @@ if(!empty($_GET['action']))
 		</nav>
 		<ul class="nav justify-content-end">
 		  <li class="nav-item">
-			<form class="form-inline my-2 my-lg-0" action="index.php" style="margin-right: 5%;">
-				<label style="font-size: 11pt;color: black;margin-top:25px;color: white;margin-right: 10px;"> Already have an account? </label><button class="btn btn-outline-primary" type="submit" style="margin-top:2px;">Sign in</button>
+			<form class="form-inline my-2 my-lg-0" action="index.php">
+				<label style="font-size: 11pt;color: black;margin-top:25px;color: white;margin-right: 10px;"> Already have an account? </label><button class="btn btn-outline-primary" type="submit" style="margin-top:2px;margin-right: 5%;">Sign in</button>
 			</form>
 		  </li>
 		</ul>

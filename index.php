873b4794d12c651ecd38890512ef37c016cb6426
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 17:55:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,107 +1,90 @@
 <?php
-include('../DB/process.php');
-
-session_start();
-if(isset($_SESSION['username'])){
+    include '/DB/process.php';
 ?>
-
 <!doctype html>
-
 <html lang="en">
-
 	<head>
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="CSS/index.css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="css/index.css">
-		<script src='https://kit.fontawesome.com/a076d05399.js'></script>
-		<link rel="shortcut icon" type="../Image/x-icon" href="images/Logo.ico">
+		<link rel="shortcut icon" type="Image/x-icon" href="Image/Logo.ico">
 		<title>Lidya Nuramo - CMS Project </title>
 	</head>
-
-	<body background="images/pattern 2.jpg">  
-  
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#">Lidya Nuramo CMS project</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		  </button>
-
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto">
-			  <li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="#">Add</a>
-			  </li>
-			  <li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				  Exercises
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-				  <a class="dropdown-item" href="../Exercise1">Exercise 1</a>
-				  <a class="dropdown-item" href="../Exercise2">Exercise 2</a>
-				  <div class="dropdown-divider"></div>
-				  <a class="dropdown-item" href="../Exercise3">Exercise 3</a>
-				</div>
-			  </li>
-			  <?php
-				if ($_SESSION['AccountType'] == "admin")
-				{ ?>
-				<li class="nav-item">
-					<a class="nav-link" href="../phpmyadmin/" target="_blank">Database</a>
-				 </li>
-				 <li class="nav-item">
-					<a class="nav-link" href="#" target="_blank">Create accounts</a>
-				 </li>
-				<?php }
-			  ?>
-			  <li class="nav-item">
-				<a class="nav-link" href="../DB/process.php?action=logout">Logout</a>
-			  </li>
-			</ul>
-			<!--form class="form-inline my-2 my-lg-0">
-			  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form-->
-		  </div>
+	
+	<body style="background: url(Image/wp3512151.jpg) no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
+		<nav class="nav text-light bg-dark">
+		  <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
 		</nav>
-		
-		<div class="all">
-			<div class="container" style="margin-top: 80px;font-size: 150%;">
-				<h1>Exercises</h1>
-				<p><h2>Student Name: Lidya Nuramo</h2></p>
-				<p><h2>Student ID: 030794</h2></p>
-				<h3>
-					<p><a href="../Exercise1">Exercies 1.</a></p>
-					<p><a href="../Exercise2">Exercies 2.</a></p>
-					<p><a href="../Exercise3">Exercies 3.</a></p>
-				</h3>
-			</div>
+		<ul class="nav justify-content-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="signup.php">Create an account</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="Guest Home">Log in as a Guest</a>
+		  </li>
+		</ul>
+		<?php
+			if(!empty($_GET['action']))
+			{
+				switch($_GET['action'])
+				{
+				  case 'yes':?>
+					<div class="form-group bg-light">
+					  <label for="exampleInputEmail1" style="color: green;" class="control-label">&nbsp;&nbsp;&nbsp;&nbsp;Account successfully created. Please log in.</label>
+					  <br>
+					</div>
+					<?php
+					break;
+				  case 'no':
+				  ?>
+
+					<div class="form-group bg-light">
+					  <label for="exampleInputEmail1" style="color: red;" class="control-label">&nbsp;&nbsp;&nbsp;&nbsp;Incorrect email or password. Please try again.</label>
+					  <br>
+					</div>
+					<?php
+					break;
+				  case 'noaccount':
+				  ?>
+
+					<div class="form-group bg-light">
+					  <label for="exampleInputEmail1" style="color: red;" class="control-label">&nbsp;&nbsp;&nbsp;&nbsp;Please log in first as you are currently not logged in.</label>
+					  <br>
+					</div>
+					<?php
+					break;
+				}
+			}
+		?>
+	
+		<div class="p-3 mb-2 text-white">
+			<form class="form-group row" action="DB/process.php?action=login" method="POST" style="background: rgba(192,192,192, 0.7);padding-top:30px;margin-top: 100px;padding-bottom:30px;height: 100%;width: 75%;margin-left: 15%;margin-bottom:100px;">
+				<div class="form-row">
+					<label style="font-size: 18pt;color: white;margin-left: 7%;font-weight: bold;" class="control-label">Log into your account</label>
+				</div>
+				<div class="form-row" style="font-size: 14pt;margin-top: 30px;">
+					<label for="exampleInputEmail1" style="font-size: 14pt;color: white;margin-left: 7%;margin-top: 5px;font-weight: bold;" class="col-sm-2 col-form-labels">Email address:</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" style="height: 40px;width: 75%;margin-left: 7%;" name="email" required>
+				</div>
+				<div class="form-row" style="font-size: 14pt;margin-top: 30px;">
+					<label for="exampleInputPassword1" style="font-size: 14pt;color: white;margin-left: 7%;margin-top: 5px;font-weight: bold;" class="col-sm-2 col-form-labels">Password:</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="height: 40px;width: 75%; margin-left: 7%;" name="password" required>
+				</div>
+				<div class="form-row" style="margin-top: 4%;margin-left: 65%;">
+					<button type="submit" class="btn btn-primary" style="width: 100px;">Submit</button>
+				</div>
+			</form>
 		</div>
+		
 		<div class="Footer" id="Footer">
 			<p align="right" style="margin-right: 10px;margin-top: 50px;color: white;"> © 2021, Lidya Nuramo </p>
 		</div>
 		
-		
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-		<script src="JS/scripts.js" type="text/javascript"></script>
 	</body>
+	
 </html>
-<?php
-}
-else{
-    header("Location: ../index.php?action=noaccount");
-}
-?>

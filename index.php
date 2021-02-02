11156565045a9575a3708878b23b71a582bd451b
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 01:16:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -65,11 +65,11 @@
 					<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" style="height: 40px;width: 700px;margin-left: 2%;" name="email" required>
 				</div>
 				<div class="form-row" style="font-size: 14pt;margin-top: 30px;">
-					<label for="exampleInputPassword1" style="font-size: 14pt;color: white;margin-left: 2%;margin-top: 5px;font-weight: bold;" class="col-sm-2 col-form-labels">Password:
-					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="height: 40px;width: 350px;" name="password" required></label>
+					<label for="exampleInputPassword1" style="font-size: 14pt;color: white;margin-left: 2%;margin-top: 5px;font-weight: bold;" class="col-sm-2 col-form-labels">Password:</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="height: 40px;width: 700px;margin-left: 2%;" name="password" required>
 				</div>
 				<br>
-				<button type="submit" class="btn btn-primary">Submit</button>
+				<button type="submit" class="btn btn-primary" style="margin-top: 20 px; align: right;">Submit</button>
 			</form>
 		</div>
 	

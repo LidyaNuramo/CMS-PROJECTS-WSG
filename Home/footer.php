b47679ae0711f0c394afa4be1c375cebbc70758c
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 21:18:05 +0100

    Update footer.php

--- Home/footer.php
+++ Home/footer.php
@@ -1,8 +1,44 @@
-		<div class="Footer" id="Footer">
-			<p align="right" style="margin-right: 10px;margin-top: 50px;color: white;"> © 2021, Lidya Nuramo </p>
-		</div>
+		<footer class="tm-bg-gray pt-5 pb-3 tm-text-gray tm-footer">
+			<div class="container-fluid tm-container-small">
+				<div class="row">
+					<div class="col-lg-6 col-md-12 col-12 px-5 mb-5">
+						<h3 class="tm-text-primary mb-4 tm-footer-title">About Project</h3>
+						<p>Student Name: Lidya Nuramo</p>
+						<p>Student ID: 030794</p>
+						<p>Student group: 3 S130Z</p>
+						<p>Course Name: Computer Measurements Systems</p>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
+						<h3 class="tm-text-primary mb-4 tm-footer-title">Our Links</h3>
+						<ul class="tm-footer-links pl-0">
+							<li><a href="mailto:lidyagnuramo@gmail.com">Contact</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
+						<ul class="tm-social-links d-flex justify-content-end pl-0 mb-5">
+							<li class="mb-2"><a href="https://facebook.com"><i class="fab fa-facebook"></i></a></li>
+							<li class="mb-2"><a href="https://twitter.com"><i class="fab fa-twitter"></i></a></li>
+							<li class="mb-2"><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></li>
+							<li class="mb-2"><a href="https://pinterest.com"><i class="fab fa-pinterest"></i></a></li>
+						</ul>
+						<a href="#" class="tm-text-gray text-right d-block mb-2">Terms of Use</a>
+						<a href="#" class="tm-text-gray text-right d-block">Privacy Policy</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-8 col-md-7 col-12 px-5 mb-3">
+						© 2021 Lidya Nuramo. All rights reserved.
+					</div>
+				</div>
+			</div>
+		</footer>
 		
-		
+		<script src="js/plugins.js"></script>
+		<script>
+			$(window).on("load", function() {
+				$('body').addClass('loaded');
+			});
+		</script>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>

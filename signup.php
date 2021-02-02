45170471581a62e6aa0ec049b0f0aad04ee0a027
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 03:54:28 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -76,6 +76,27 @@ if(!empty($_GET['action']))
 		  </div>
 		</form>
 		
+		<script>
+			function check_pass() {
+				var password=document.getElementById('InputPassword1');
+				var password2=document.getElementById('InputPassword2');
+
+				if (password.value.length <6 ){
+					window.alert("Password length is less than 6.");
+					return false;
+				}
+				else{
+					if (password.value == password2.value) {
+						return true;
+					} else {
+						window.alert("Passwords don't match.");
+						return false;
+					}
+				}
+				
+			}
+		</script>
+		
 		<div class="Footer" id="Footer">
 			<p align="right" style="margin-right: 10px;margin-top: 50px;color: white;"> © 2020, Lidya Nuramo </p>
 		</div>
@@ -83,7 +104,7 @@ if(!empty($_GET['action']))
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-		<script src="js/scripts.js" type="text/javascript"></script>
+		<script src="JS/scripts.js" type="text/javascript"></script>
 		
 	</body>
 

4e0a17a949467f2b2f0be866a56ff5236c43d7da
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 05:04:05 +0100

    Update index.php

--- Home/index.php
+++ Home/index.php
@@ -1,80 +1,75 @@
-<?php 
-  session_start();
-  if(isset($_SESSION['username'])){
+<?php
+include('../DB/process.php');
+
+session_start();
+if(isset($_SESSION['username'])){
 ?>
 
 <!doctype html>
 
 <html lang="en">
 
-  <head>
+	<head>
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="CSS/index.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<script src='https://kit.fontawesome.com/a076d05399.js'></script>
 		<title>Lidya Nuramo - CMS Project </title>
-  </head>
+	</head>
 
-  <body background="images/pattern 2.jpg">  
-    <section style="font-size: 15pt;color:black;">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
-        <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="home.php">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Exercies
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="add.php">Exercise 1</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="view.php">Exercise 2</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Account
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="profile.php">Change password</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="process.php?action=logout">Log Out</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.php">About</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0" action="search.php" method="POST">
-            <font color="black" size="3">Keyword: </font>
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id='search' name="Keyword" required>
-            <font color="black" size="3">Type: </font>
-            <select class="form-control" id="exampleFormControlSelect1" name="type" required>
-              <option value="Name" >Name</option>
-              <option value="CityName" >City</option>
-            </select>
-            <input type="submit" class="btn btn-outline-primary" name="submit" value="Search">
-          </form>
-        </div>
-      </nav>
-    </section> 
+	<body background="images/pattern 2.jpg">  
+  
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">Lidya Nuramo CMS project</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Add</a>
+			  </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  Exercies
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				  <a class="dropdown-item" href="../Exercise1">Exercise 1</a>
+				  <a class="dropdown-item" href="../Exercise2">Exercise 2</a>
+				  <div class="dropdown-divider"></div>
+				  <a class="dropdown-item" href="../Exercise3">Exercise 3</a>
+				</div>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="../DB/process.php?action=logout">Logout</a>
+			  </li>
+			</ul>
+			<!--form class="form-inline my-2 my-lg-0">
+			  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form-->
+		  </div>
+		</nav>
+		
 		<div class="all">
-			<div class="container" style="margin-top: 80px;margin-left:-100px;font-size: 200%;">
+			<div class="container" style="margin-top: 80px;font-size: 150%;">
 				<h1>Exercises</h1>
 				<p><h2>Student Name: Lidya Nuramo</h2></p>
 				<p><h2>Student ID: 030794</h2></p>
 				<h3>
-					<p><a href="Exercise1">Exercies 1.</a></p>
-					<p><a href="Exercise2">Exercies 2.</a></p>
-					<p><a href="Exercise3">Exercies 3.</a></p>
+					<p><a href="../Exercise1">Exercies 1.</a></p>
+					<p><a href="../Exercise2">Exercies 2.</a></p>
+					<p><a href="../Exercise3">Exercies 3.</a></p>
 				</h3>
 			</div>
 		</div>
@@ -82,6 +77,10 @@
 			<p align="right" style="margin-right: 10px;margin-top: 50px;color: white;"> © 2021, Lidya Nuramo </p>
 		</div>
 		
+		
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>

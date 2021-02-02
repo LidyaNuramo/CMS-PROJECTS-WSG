917e26313c5272d16c05323f9ea117efe90ca454
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 21:34:01 +0100

    Update footer.php

--- Home/footer.php
+++ Home/footer.php
@@ -9,7 +9,7 @@
 						<p>Course Name: Computer Measurements Systems</p>
 					</div>
 					<div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
-						<h3 class="tm-text-primary mb-4 tm-footer-title">Our Links</h3>
+						<h3 class="tm-text-primary mb-4 tm-footer-title">Links</h3>
 						<ul class="tm-footer-links pl-0">
 							<li><a href="mailto:lidyagnuramo@gmail.com">Contact</a></li>
 						</ul>
@@ -21,13 +21,9 @@
 							<li class="mb-2"><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></li>
 							<li class="mb-2"><a href="https://pinterest.com"><i class="fab fa-pinterest"></i></a></li>
 						</ul>
-						<a href="#" class="tm-text-gray text-right d-block mb-2">Terms of Use</a>
-						<a href="#" class="tm-text-gray text-right d-block">Privacy Policy</a>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-lg-8 col-md-7 col-12 px-5 mb-3">
-						© 2021 Lidya Nuramo. All rights reserved.
+						<div class="col-lg-8 col-md-7 col-12 px-5 mb-3">
+							© 2021 Lidya Nuramo. All rights reserved.
+						</div>
 					</div>
 				</div>
 			</div>

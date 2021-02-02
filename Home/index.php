4e2ff71bcef94e975d8b9b9176feb79bdb079625
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 21:39:15 +0100

    Update index.php

--- Home/index.php
+++ Home/index.php
@@ -61,20 +61,7 @@ include('header.php');
 				</div>
 			</div> <!-- row -->
 		</div> <!-- container-fluid, tm-container-content -->
-
 		
-		<div class="all">
-			<div class="container" style="margin-top: 80px;font-size: 150%;">
-				<h1>Exercises</h1>
-				<p><h2>Student Name: Lidya Nuramo</h2></p>
-				<p><h2>Student ID: 030794</h2></p>
-				<h3>
-					<p><a href="../Exercise1">Exercies 1.</a></p>
-					<p><a href="../Exercise2">Exercies 2.</a></p>
-					<p><a href="../Exercise3">Exercies 3.</a></p>
-				</h3>
-			</div>
-		</div>
 <?php
 	include('footer.php');
 ?>

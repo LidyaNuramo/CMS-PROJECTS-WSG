a1e9fc974a188e0acd567052f500671778440540
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 21:46:06 +0100

    Update header.php

--- Home/header.php
+++ Home/header.php
@@ -70,7 +70,7 @@ if(isset($_SESSION['username'])){
 		
 	</head>
 	<body style="background: url(../Image/bg2.png) no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"> 
-		<!div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="../Image/bg2.jpg">
+		<div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="../Image/bg2.jpg">
 			<form class="d-flex tm-search-form">
 				<input class="form-control tm-search-input" type="search" placeholder="Search" aria-label="Search">
 				<button class="btn btn-outline-success tm-search-btn" type="submit">

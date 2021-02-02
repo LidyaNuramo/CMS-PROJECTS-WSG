3a57aa21b68358687dd6a49f88d6aec1080abb6c
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 02:25:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<title>Lidya Nuramo - CMS Project </title>
 	</head>
 	
-	<body style="background-image: url('Image/wp3512151.jpg'); background-size: cover;">
+	<body style="background: url(Image/wp3512151.jpg) no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
 		<nav class="nav text-light bg-dark">
 		  <a class="nav-link active" aria-current="page" href="#">Lidya Nuramo CMS project</a>
 		</nav>

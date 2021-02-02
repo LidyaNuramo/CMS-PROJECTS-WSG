2ae4db4c76420b5e97fb13897edf4dcf1fd9d989
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-02 18:29:43 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -22,7 +22,7 @@ if(!empty($_GET['action']))
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-		<link rel="shortcut icon" type="Image/x-icon" href="Image/Logo.ico">
+		<link rel="shortcut icon" type="Image/x-icon" href="Image/logo.ico">
 		<title>Lidya Nuramo - CMS Project </title>
 	</head>
 

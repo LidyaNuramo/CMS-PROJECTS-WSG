d60e2bb25aa6309bac897df9bab328e252a03eaf
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-03 05:46:06 +0100

    Update index1.php

--- Exercise2/index1.php
+++ Exercise2/index1.php
@@ -8,7 +8,7 @@ $database= new Database();
 	<br>
 	<br>
 	<div id="container">
-		<div class="btn-group-vertical" style="margin-left: -400px;">
+		<div class="btn-group-vertical" style="margin-left: -350px;">
 			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index.php</button></a>
 			<a href="#"><button type="button" class="btn btn-secondary" style="width:150px;">Index1.php</button></a>
 			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index2.php</button></a>
@@ -227,7 +227,7 @@ if ((isset($_REQUEST['rss']) && ($rssfeed))
 echo $htmlheader;
 
 // headline, date and time and start of table
-echo "<h1 style='margin-top: -100px;'>$headline</h1>\n";
+echo "<h1 style='margin-top: -150px;'>$headline</h1>\n";
 if ($datetime) echo '<p class="datetime" style="color: black">As of ' . date($datetime) . "</p>\n";
 
 // show the contents of $alertfile if it is readable and larger than 7 bytes

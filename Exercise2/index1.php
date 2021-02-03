505d93bc82a338315690a299c8f982c1e5f177ea
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-03 04:25:26 +0100

    Update index1.php

--- Exercise2/index1.php
+++ Exercise2/index1.php
@@ -7,10 +7,10 @@ include('../header.php');
 	<br>
 	<br>
 	<div id="container">
-		<div class="btn-group-vertical" style="margin-left: -50px;">
-			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:100px;">Index.php</button></a>
-			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:100px;">Index1.php</button>
-			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:100px;">Index2.php</button>
+		<div class="btn-group-vertical" style="margin-left: -350px;">
+			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index.php</button></a>
+			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index1.php</button>
+			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index2.php</button>
 			<!--div class="btn-group" role="group">
 				<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				  Dropdown

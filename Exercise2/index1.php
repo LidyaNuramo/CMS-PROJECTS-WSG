cf518b8e02232729beaf3aef3531675fecc94041
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-03 05:42:01 +0100

    Update index1.php

--- Exercise2/index1.php
+++ Exercise2/index1.php
@@ -8,7 +8,7 @@ $database= new Database();
 	<br>
 	<br>
 	<div id="container">
-		<div class="btn-group-vertical" style="margin-left: -350px;">
+		<div class="btn-group-vertical" style="margin-left: -400px;">
 			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index.php</button></a>
 			<a href="#"><button type="button" class="btn btn-secondary" style="width:150px;">Index1.php</button></a>
 			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index2.php</button></a>

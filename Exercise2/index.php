e542145df852f6d92cfe6841400a7cbe5d99b6d3
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-03 04:16:27 +0100

    Update index.php

--- Exercise2/index.php
+++ Exercise2/index.php
@@ -3,20 +3,23 @@
 	include('../DB/process.php');
 	include('../header.php');
 ?>
-	<div class="btn-group-vertical">
-		<a href="#"><button type="button" class="btn btn-secondary" style="width:100px;margin-top: 50 px;">Index.php</button></a>
-		<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:100px;">Index1.php</button>
-		<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:100px;">Index2.php</button>
-		<!--div class="btn-group" role="group">
-			<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			  Dropdown
-			</button>
-			<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-			  <a class="dropdown-item" href="#">Dropdown link</a>
-			  <a class="dropdown-item" href="#">Dropdown link</a>
-			</div>
-		</div-->
-	</div>	
+	
+	<div id="container">
+		<div class="btn-group-vertical" style="margin-top: 50 px; margin-left: 20px;">
+			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index.php</button></a>
+			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index1.php</button>
+			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index2.php</button>
+			<!--div class="btn-group" role="group">
+				<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  Dropdown
+				</button>
+				<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+				  <a class="dropdown-item" href="#">Dropdown link</a>
+				  <a class="dropdown-item" href="#">Dropdown link</a>
+				</div>
+			</div-->
+		</div>
+	</div>
 	<br>
 	<br>
 	<br>

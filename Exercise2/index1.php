702769b0dcd5927924ccc321a7efe21b96bba87a
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-03 04:19:42 +0100

    Update index1.php

--- Exercise2/index1.php
+++ Exercise2/index1.php
@@ -2,6 +2,26 @@
 include '../DB/main.php';
 include('../DB/process.php');
 include('../header.php');
+
+	<br>
+	<br>
+	<br>
+	<div id="container">
+		<div class="btn-group-vertical" style="margin-left: 20px;">
+			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index.php</button></a>
+			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index1.php</button>
+			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index2.php</button>
+			<!--div class="btn-group" role="group">
+				<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  Dropdown
+				</button>
+				<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+				  <a class="dropdown-item" href="#">Dropdown link</a>
+				  <a class="dropdown-item" href="#">Dropdown link</a>
+				</div>
+			</div-->
+		</div>
+	</div>
 /**
  * netstat.php - Show online status of hosts and services
  *

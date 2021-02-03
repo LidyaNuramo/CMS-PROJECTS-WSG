660ac2582a0f56db2da1da8c0c5caf61045b0a14
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-02-03 03:54:44 +0100

    Update index.php

---
+++ Exercise2/index.php
@@ -0,0 +1,22 @@
+<?php
+    include '../DB/main.php';
+	include('../DB/process.php');
+	include('../header.php');
+?>
+	<div class="btn-group-vertical">
+		<a href="#"><button type="button" class="btn btn-secondary">Index.php</button></a>
+		<a href="Index1.php"><button type="button" class="btn btn-secondary">Index1.php</button>
+		<a href="Index2.php"><button type="button" class="btn btn-secondary">Index2.php</button>
+		<!--div class="btn-group" role="group">
+			<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			  Dropdown
+			</button>
+			<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+			  <a class="dropdown-item" href="#">Dropdown link</a>
+			  <a class="dropdown-item" href="#">Dropdown link</a>
+			</div>
+		</div-->
+	</div>	
+<?php
+	include('../footer.php');
+?>

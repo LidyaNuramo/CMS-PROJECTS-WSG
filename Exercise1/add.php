c10d89b4f99e0df5afefd28d0d2f8880ce93be9b
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-11 20:59:39 +0100

    .

--- Exercise1/add.php
+++ Exercise1/add.php
@@ -24,7 +24,7 @@
 
 		  <div class="form-row">
 			<label for="name" class="col-sm-2 col-form-label" style="font-size: 14pt; margin-top: 30px;" required>Assigned to:</lable>
-				<select class="form-control" name="assignedto" style="height: 100 px; width: 700px;">
+				<select class="form-control" name="assignedto" style="height: 100%; width: 100%;">
 				  <option selected value=""> </option>
 		      		<?php
 			    	$database=new Database();

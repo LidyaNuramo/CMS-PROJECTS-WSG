bcdab477c3618208d7248d4c8057b963b6c845f2
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-11 22:50:37 +0100

    .

--- Exercise2/index2.php
+++ Exercise2/index2.php
@@ -3,11 +3,8 @@ include '../DB/main.php';
 include('../DB/process.php');
 include('../header.php');
 ?>
-	<br>
-	<br>
-	<br>
 	<div id="container">
-		<div class="btn-group-vertical" style="margin-left: 5px;">
+		<div class="btn-group-vertical" style="margin-left: 5px;margin-top: 100px;">
 			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index.php</button></a>
 			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index1.php</button></a>
 			<a href="#"><button type="button" class="btn btn-secondary" style="width:150px;">Index2.php</button></a>

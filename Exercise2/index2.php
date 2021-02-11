3365cb0c07e29ef8d9d8b3ec2010dd0429d4a938
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-11 22:43:34 +0100

    .

--- Exercise2/index2.php
+++ Exercise2/index2.php
@@ -7,14 +7,16 @@ include('../header.php');
 	<br>
 	<br>
 	<div id="container">
-		<div class="btn-group-vertical" style="margin-left: 350px;">
+		<div class="btn-group-vertical" style="margin-left: 100px;">
 			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index.php</button></a>
 			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index1.php</button></a>
 			<a href="#"><button type="button" class="btn btn-secondary" style="width:150px;">Index2.php</button></a>
 		</div>
 	</div>
 	
-
+	<?php
+		echo phpinfo ();
+	?>
 	
 	<br>
 	<br>

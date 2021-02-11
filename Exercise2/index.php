fad90e2fe591c22654fd85d7958b82692e840428
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-11 22:40:27 +0100

    .

--- Exercise2/index.php
+++ Exercise2/index.php
@@ -8,18 +8,9 @@
 	<br>
 	<div id="container">
 		<div class="btn-group-vertical" style="margin-left: 20px;">
-			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index.php</button></a>
+			<a href="#"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index.php</button></a>
 			<a href="index1.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index1.php</button></a>
 			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:100px;margin-left: 30px;">Index2.php</button></a>
-			<!--div class="btn-group" role="group">
-				<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				  Dropdown
-				</button>
-				<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-				  <a class="dropdown-item" href="#">Dropdown link</a>
-				  <a class="dropdown-item" href="#">Dropdown link</a>
-				</div>
-			</div-->
 		</div>
 	</div>
 	<div id="container">
--- Exercise2/index1.php
+++ Exercise2/index1.php
@@ -12,15 +12,6 @@ $database= new Database();
 			<a href="index.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index.php</button></a>
 			<a href="#"><button type="button" class="btn btn-secondary" style="width:150px;">Index1.php</button></a>
 			<a href="index2.php"><button type="button" class="btn btn-secondary" style="width:150px;">Index2.php</button></a>
-			<!--div class="btn-group" role="group">
-				<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				  Dropdown
-				</button>
-				<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-				  <a class="dropdown-item" href="#">Dropdown link</a>
-				  <a class="dropdown-item" href="#">Dropdown link</a>
-				</div>
-			</div-->
 		</div>
 	</div>
 <?php

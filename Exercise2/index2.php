d1113a50c4b006e8f47d019804cbb7c435ca7267
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 00:13:48 +0100

    .

--- Exercise2/index2.php
+++ Exercise2/index2.php
@@ -65,13 +65,28 @@ include('../header.php');
 	?>
 	<br>
 
-	<label for="name" class="col-sm-2 col-form-label">DNS Info:</label>
-	<br>
 	<?php
 	$time1 = time ();
 	$time2 = date ("r", $time1);
 	echo 'Unix time: ' . $time1 . "<br>";
 	echo 'Readable format: ' . $time2;
+	?>
+	<br>
+
+	<label for="name" class="col-sm-2 col-form-label">User IP Location:</label>
+	<?php
+	$ipaddress = $_SERVER["REMOTE_ADDR"];
+	function ip_details($ip) {
+	$json = file_get_contents("http://ipinfo.io/{$ip}/geo");
+	$details = json_decode ($json);
+	return $details;
+	}
+	$details = ip_details($ipaddress);
+	echo $details -> region; echo '<BR />';
+	echo $details -> country; echo '<BR />';
+	echo $details -> city; echo '<BR />';
+	echo $details -> loc; echo '<BR />';
+	echo $details -> ip; echo '<BR />';
 	?>
 
 	<br>

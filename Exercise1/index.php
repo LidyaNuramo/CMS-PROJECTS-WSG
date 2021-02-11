dd47bbc6e0d878aa820e3497913ecf150b4e9b42
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-11 15:02:36 +0100

    .

--- Exercise1/index.php
+++ Exercise1/index.php
@@ -7,22 +7,22 @@
 		<meta http-equiv="refresh" content="10" />
 		<div class="row" style="max-width:75%;">
             <div class="col-sm-12">
-                <table class="table table-striped" border="5" style="margin-top: 70px;margin-left: 2%;max-width:75%;">
+                <table class="table table-striped" border="5" style="margin-top: 70px;margin-left: 2%;width:100%;">
 				<thead>
 					<tr class="thead-dark" style="font-size: 16pt;white-space:nowrap;">
 						<th scope="col" colspan="10"><p class="h1" style="color: white;">Monitored hosts</p></th>
 					</tr>
                     <tr class="thead-dark" style="font-size: 16pt;white-space:nowrap;">
-                        <th scope="col">#</th>
-                        <th scope="col">Address</th>
-                        <th scope="col">Port</th>
-						<th scope="col">Status</th>
-                        <th scope="col" style="max-width:20%;">Reconnection attempts</th>
-                        <th scope="col" style="max-width:50%;">Connection loss time </th>
-                        <th scope="col" style="max-width:40%;">Total downtime</th>
-						<th scope="col">Added by</th>
-						<th scope="col">Assigned to</th>
-                        <th scope="col">Action</th>
+                        <th scope="col" style="width:10%;">#</th>
+                        <th scope="col" style="width:10%;">Address</th>
+                        <th scope="col" style="width:10%;">Port</th>
+						<th scope="col" style="width:10%;">Status</th>
+                        <th scope="col" style="width:10%;">Reconnection attempts</th>
+                        <th scope="col" style="width:10%;">Connection loss time </th>
+                        <th scope="col" style="width:10%;">Total downtime</th>
+						<th scope="col" style="width:10%;">Added by</th>
+						<th scope="col" style="width:10%;">Assigned to</th>
+                        <th scope="col" style="width:10%;">Action</th>
                     </tr>
                 </thead>
                 <tbody style="font-size: 12pt;color: black;font-weight: bold;background: rgba(192,192,192, 0.7);">

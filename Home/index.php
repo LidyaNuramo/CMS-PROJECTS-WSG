9896aebf1b36395427ef826ec291b65e462b2ae3
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 23:24:25 +0100

    .

--- Home/index.php
+++ Home/index.php
@@ -46,6 +46,15 @@ include('../header.php');
 						</figcaption>                    
 					</figure>
 				</div>
+				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
+					<figure class="effect-ming tm-video-item">
+						<img src="../Image/ex4.jpg" alt="Image" class="img-fluid"  style="width: 300px; height: 170px;">
+						<figcaption class="d-flex align-items-center justify-content-center">
+							<h2>Exercise 6</h2>
+							<a href="../Exercise6" target="_blank">View more</a>
+						</figcaption>                    
+					</figure>
+				</div>
 			</div> <!-- row -->
 		</div> <!-- container-fluid, tm-container-content -->
 		

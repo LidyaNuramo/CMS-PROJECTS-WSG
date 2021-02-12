5dd18f8fede8e00879bc507fc88ac91e6e2c8f79
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 18:11:52 +0100

    .

--- Exercise4/index.php
+++ Exercise4/index.php
@@ -10,7 +10,7 @@
 	<br>
 
 
-	
+	<img src="https://lidyanuramo.com/Exercise4/graph.php" style="-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300m">
 
 
 	<br>

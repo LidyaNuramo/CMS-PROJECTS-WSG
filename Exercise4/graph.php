75889662874a2a8aa64085dd92a869513dc69204
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 20:19:10 +0100

    .

--- Exercise4/graph.php
+++ Exercise4/graph.php
@@ -9,23 +9,16 @@
 	$plot = new PHPlot();
 	$data = array();
 	foreach($results as $result){
-		$result['nick'];
-		$result['message']; 
-		$result['datetime']; 
-		$title = "Line ".$num;
+		$title = "Line ".$num." at ".$result['datetime'];
 		$num = $num + 1;
-		$data2 = array( array($title,0,$result['x1']),array('',1,$result['x2']),array('',2,$result['x3']),array('',3,$result['x4']), array('',4,$result['x5']),);
-		//array_push($data,$data2);
-		$plot->SetDataValues($data2);
-		$plot->SetDataType('data-data');
-		$plot->SetTitle('Generated graph from Database');
-		$plot->DrawGraph();
+		$data2 = array($title,1,$result['x1'],$result['x2'],$result['x3'],$result['x4'],$result['x5']);
+		array_push($data,$data2);
 		if ($num == 6){
 			break;
 		}
 	}
-	//$plot->SetDataValues($data);
-	//$plot->SetDataType('data-data');
-	//$plot->SetTitle('Generated graph from Database');
-	//$plot->DrawGraph();
+	$plot->SetDataValues($data);
+	$plot->SetDataType('data-data');
+	$plot->SetTitle('Generated graph');
+	$plot->DrawGraph();
 ?>

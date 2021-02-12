9ba8405a37718756a30d02137d2cab16dd1aa926
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 19:30:07 +0100

    .

--- Exercise4/graph.php
+++ Exercise4/graph.php
@@ -15,12 +15,13 @@
 		$title = "Line ".$num;
 		$num = $num + 1;
 		$data2 = array( array($title,0,$result['x1']),array('',1,$result['x2']),array('',2,$result['x3']),array('',3,$result['x4']), array('',4,$result['x5']),);
-		array_push($data,$data2);
+		//array_push($data,$data2);
+		$plot->SetDataValues($data2);
 		if ($num == 6){
 			break;
 		}
 	}
-	$plot->SetDataValues($data);
+	//$plot->SetDataValues($data);
 	$plot->SetDataType('data-data');
 	$plot->SetTitle('Generated graph from Database');
 	$plot->DrawGraph();

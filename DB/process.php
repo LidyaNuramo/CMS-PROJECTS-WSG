b6a4ee9a2157ad778cdb3841815611d5b0bd5715
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 20:29:45 +0100

    .

--- DB/process.php
+++ DB/process.php
@@ -130,10 +130,10 @@ if(!empty($_GET['action'])){
 			$data=array(
 				"id"=>null,
 				"x1"=>$x1,
-				"x2"=>$x1,
-				"x3"=>$x1,
-				"x4"=>$x1,
-				"x5"=>$x1,
+				"x2"=>$x2,
+				"x3"=>$x3,
+				"x4"=>$x4,
+				"x5"=>$x5,
 				"datetime"=>$time,
 			);
 			$database->insertRows("measurements",$data);
--- Exercise4/index.php
+++ Exercise4/index.php
@@ -14,49 +14,51 @@
 	<br>
 	<br>
 
-	<table  class="table table-bordered table-secondary" style="margin-left: 200 px;width: 50%">
-		<tbody>
-            <thead class="thead-dark">
+	<div class="container" style="margin-left: 100 px;">
+		<table  class="table table-bordered table-secondary" style="margin-left: 200 px;width: 50%">
+			<tbody>
+				<thead class="thead-dark">
+					<tr>
+						<th colspan='2'>Add to graph:</th>
+					</tr>
+				</thead>
 				<tr>
-					<th colspan='2'>Add to graph:</th>
+					<td colspan="2">
+						<form class="form-group row" method="POST" action="../DB/process.php?action=addgraph">
+							<div class="form-row" style="font-size: 14pt;">
+								<label for="name" class="col-sm-2 col-form-label">X1:</label>
+								<input type="text" name="x1" maxlength="10" size="10" required>
+							</div>
+							<br>
+							<div class="form-row" style="font-size: 14pt;">
+								<label for="name" class="col-sm-2 col-form-label">X2:</label>
+								<input type="text" name="x2" maxlength="10" size="10" required>
+							</div>
+							<br>
+							<div class="form-row" style="font-size: 14pt;">
+								<label for="name" class="col-sm-2 col-form-label">X3:</label>
+								<input type="text" name="x3" maxlength="10" size="10" required>
+							</div>
+							<br>
+							<div class="form-row" style="font-size: 14pt;">
+								<label for="name" class="col-sm-2 col-form-label">X4:</label>
+								<input type="text" name="x4" maxlength="10" size="10" required>
+							</div>
+							<br>
+							<div class="form-row" style="font-size: 14pt;">
+								<label for="name" class="col-sm-2 col-form-label">X5:</label>
+								<input type="text" name="x5" maxlength="10" size="10" required>
+							</div>
+							<br>
+							<div class="form-row">
+								<button type="submit" class="btn btn-primary" id="submit">Submit</button>
+							</div>
+						</form>
+					</td>
 				</tr>
-			</thead>
-			<tr>
-				<td colspan="2">
-					<form class="form-group row" method="POST" action="../DB/process.php?action=addgraph">
-						<div class="form-row" style="font-size: 14pt;">
-							<label for="name" class="col-sm-2 col-form-label">X1:</label>
-							<input type="text" name="x1" maxlength="10" size="10" required>
-						</div>
-						<br>
-						<div class="form-row" style="font-size: 14pt;">
-							<label for="name" class="col-sm-2 col-form-label">X2:</label>
-							<input type="text" name="x2" maxlength="10" size="10" required>
-						</div>
-						<br>
-						<div class="form-row" style="font-size: 14pt;">
-							<label for="name" class="col-sm-2 col-form-label">X3:</label>
-							<input type="text" name="x3" maxlength="10" size="10" required>
-						</div>
-						<br>
-						<div class="form-row" style="font-size: 14pt;">
-							<label for="name" class="col-sm-2 col-form-label">X4:</label>
-							<input type="text" name="x4" maxlength="10" size="10" required>
-						</div>
-						<br>
-						<div class="form-row" style="font-size: 14pt;">
-							<label for="name" class="col-sm-2 col-form-label">X5:</label>
-							<input type="text" name="x5" maxlength="10" size="10" required>
-						</div>
-						<br>
-						<div class="form-row">
-							<button type="submit" class="btn btn-primary" id="submit">Submit</button>
-						</div>
-					</form>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+	</div>
 
 	<br>
 	<br>

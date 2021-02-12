73e65270534cff456a89263707c50e7e22df4d69
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 02:16:55 +0100

    .

--- header.php
+++ header.php
@@ -71,23 +71,11 @@ if(isset($_SESSION['username'])){
 				<a class="nav-link" href="../DB/process.php?action=logout">Logout</a>
 			  </li>
 			</ul>
-			<!--form class="form-inline my-2 my-lg-0">
-			  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form-->
 		  </div>
 		</nav>
 		
 	</head>
-	<body style="background: url(../Image/bg2.png) no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"> 
-		<!--div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="../Image/bg3.jpg" style="background: url(../Image/bg3.jpg);">
-			<form class="d-flex tm-search-form">
-				<input class="form-control tm-search-input" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success tm-search-btn" type="submit">
-					<i class="fas fa-search"></i>
-				</button>
-			</form>
-		</div-->
+	<body style="background: url(../Image/bg2.png) no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
 <?php
 }
 else{

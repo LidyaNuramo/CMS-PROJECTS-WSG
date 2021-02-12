daa58d596edd56dd0ebaf9030d3c171b18f572a2
LidyaNuramo <LidyaNuramo@users.noreply.github.com>
2021-02-12 02:44:06 +0100

    .

--- DB/process.php
+++ DB/process.php
@@ -108,10 +108,10 @@ if(!empty($_GET['action'])){
 				"message"=>$time,
 			);
 			$database->insertRows("messages",$data);
-			session_start();
-			if(isset($_SESSION['nickname'])){
-				$_SESSION['nickname'])= $user;
-			}
+			//session_start();
+			//if(isset($_SESSION['nickname'])){
+			//	$_SESSION['nickname'])= $user;
+			//}
 			//$text = '<table  border=”1” width="90%"><tr><td>'.$post.'</td><td width="80">'.$user.'</td><td width="60" bgcolor="yellow">'.$time.'</td></tr></table><br>';
 			//$file = fopen ("conversation.txt", "a+");
 			//fwrite ($file, $text);
